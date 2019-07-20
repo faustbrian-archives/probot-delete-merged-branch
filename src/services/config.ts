4d55446b647594d485aaa2969b01c11a52f404b0
Brian Faust <hello@basecode.sh>
2019-07-20 15:54:37 +0300

    chore(deps): bump dependencies

--- src/services/config.ts
+++ src/services/config.ts
@@ -5,7 +5,6 @@ import { Context } from "probot";
 export const loadConfig = async (context: Context): Promise<Record<string, any>> =>
 	(await getConfig(
 		context,
-		"botamic.yml",
 		Joi.object({
 			deleteMergedBranch: Joi.object({
 				exclude: Joi.array()
@@ -18,4 +17,4 @@ export const loadConfig = async (context: Context): Promise<Record<string, any>>
 		})
 			.unknown(true)
 			.default(),
-	)).deleteMergedBranch;
+	));
